0c8637e26c9c308da770f20c02327527b5576c48
Justin Swanson <geeksunny@gmail.com>
2015-06-22 14:43:06 -0500

    Fixing body text posting.

--- app/src/main/java/com/radicalninja/restoid/data/rest/adapter/RestAdapter.java
+++ app/src/main/java/com/radicalninja/restoid/data/rest/adapter/RestAdapter.java
@@ -2,15 +2,21 @@ package com.radicalninja.restoid.data.rest.adapter;
 
 import android.util.Pair;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 import com.radicalninja.restoid.application.App;
-import com.radicalninja.restoid.data.rest.LenientGsonConverter;
 import com.radicalninja.restoid.data.rest.client.RestClient;
 import com.radicalninja.restoid.data.rest.interceptor.RestInterceptor;
 
+import java.io.IOException;
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
+
+import okio.ByteString;
+import retrofit.converter.ConversionException;
+import retrofit.converter.Converter;
+import retrofit.mime.TypedInput;
+import retrofit.mime.TypedOutput;
+import retrofit.mime.TypedString;
 
 public class RestAdapter {
 
@@ -20,12 +26,7 @@ public class RestAdapter {
     private retrofit.RestAdapter mRestAdapter = null;
     private RestClient mRestClient = null;
 
-    protected List<Pair<String, String>> mRequiredHeaders =
-            new ArrayList<Pair<String, String>>() {
-                {
-                    add(new Pair<>("Accept", "application/json"));
-                }
-            };
+    protected List<Pair<String, String>> mRequiredHeaders = new ArrayList<>();
 
     public RestAdapter() {
         //
@@ -46,24 +47,31 @@ public class RestAdapter {
     }
 
     protected void createAdapter() {
-        // GSON parser
-        GsonBuilder gsonBuilder = new GsonBuilder();
-        gsonBuilder.excludeFieldsWithoutExposeAnnotation();
-        Gson gson = gsonBuilder.create();
-
         // Rest Adapter
         sRestInterceptor = RestInterceptor.getInstance();
         mRestAdapter = new retrofit.RestAdapter.Builder()
                 .setEndpoint(App.getEndpoint())
                 .setRequestInterceptor(sRestInterceptor)
                 .setLogLevel(retrofit.RestAdapter.LogLevel.FULL)
-                //.setConverter(new GsonConverter(gson))
-                .setConverter(new LenientGsonConverter(gson))
+                .setConverter(new StringConverter())
                 .build();
         mRestClient = mRestAdapter.create(RestClient.class);
 
-        for(Pair<String, String> headerPair : mRequiredHeaders) {
-            sRestInterceptor.addHeader(headerPair.first, headerPair.second);
-        }
+//        for(Pair<String, String> headerPair : mRequiredHeaders) {
+//            sRestInterceptor.addHeader(headerPair.first, headerPair.second);
+//        }
     }
-}
+
+    public class StringConverter implements Converter {
+        @Override public Object fromBody(TypedInput body, Type type) throws ConversionException {
+            try {
+                return ByteString.read(body.in(), (int) body.length()).utf8();
+            } catch (IOException e) {
+                throw new ConversionException("Problem when convert string", e);
+            }
+        }
+
+        @Override public TypedOutput toBody(Object object) {
+            return new TypedString((String) object);
+        }
+    }}
